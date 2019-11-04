74f1cd771e4b90696085b7233f04bae4d42400a8
Darragh Coy <darraghcoy@runbox.com>
2019-11-03 19:21:43 -0800

    Define all the data structures involved in the object file.

--- psxobj_siggen/Main.cpp
+++ psxobj_siggen/Main.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <fstream>
 
 //----------------------------------------------------------------------------------------------------------------------
 // Entry point for 'PSXObjSigGen'
@@ -7,12 +6,12 @@
 // Program purpose:
 //
 //  This program parses the human readable textual output of the 'DUMPOBJ.EXE' tool included in the PlayStation SDK 
-//  (PsyQ SDK) when invoked with the '/c' option (print code and data). Based on this dump output, it generates both
-//  function and data signatures for the contents of the .OBJ file as well as reference disassembly for code.
+//  (PsyQ SDK) when invoked with the '/c' option (print code and data). Based on this dump output, it generates function
+//  signatures for the contents of the .OBJ file as well as reference disassembly for code.
 //
-//  These signatures and disassembly are then appended to specified output files, so the results of many invocations can
-//  be gathered together to form one database of all this info. This database can then be used to try and identify the
-//  same functions and data within a given PlayStation executable.
+//  These signatures are then appended to specified output files, so the results of many invocations can be gathered
+//  together to form one database of all this info. This database can then be used to try and identify the same
+//  functions within a given PlayStation executable.
 //
 //  Basically this tool was created so we can identify PsyQ SDK constructs within a PlayStation executable.
 //
@@ -21,8 +20,9 @@
 //          For more info, see: https://www.retroreversing.com/ps1-psylink
 //      (2) Invoke 'DUMPOBJ.EXE' on every SDK .OBJ file to produce the textual output that is an input to this tool.
 //      (3) Use this tool on all the dump output to build up the signature database.
-//      (4) Run pattern matching on the .EXE using the generated signature database to identify PsyQ constructs in an EXE.
+//      (4) Run pattern matching on the .EXE using the generated signature database to identify PsyQ functions in an EXE.
 //----------------------------------------------------------------------------------------------------------------------
+
 int main(int argc, char* argv[]) noexcept {
     return 0;
 }
