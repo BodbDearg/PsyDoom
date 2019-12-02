bd16e6925c380890fd4a650e7059b504be573234
Darragh Coy <darraghcoy@runbox.com>
2019-12-02 01:29:47 -0800

    CMake file for nlohmann-json

---
+++ third_party_libs/nlohmann-json/Dummy.cpp
@@ -0,0 +1,3 @@
+// This file exists so we can treat this header only library as a regular library in various build systems.
+// I'm just including this header to test everything is ok...
+#include <nlohmann/json.hpp>
