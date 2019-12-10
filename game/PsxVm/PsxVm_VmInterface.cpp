ba9dd0058dfb1f13932dbf82c53b9a27f86906ac
Darragh Coy <darraghcoy@runbox.com>
2019-12-09 21:05:41 -0800

    Handle div instructiuons and sub

--- game/PsxVm/PsxVm_VmInterface.cpp
+++ game/PsxVm/PsxVm_VmInterface.cpp
@@ -9,6 +9,8 @@
 #include <cstdlib>
 #include <system.h>
 
+using namespace PsxVm;
+
 namespace PsxVm {
     const uint32_t* gpReg_zero;
     uint32_t* gpReg_at;
@@ -75,11 +77,32 @@ uint32_t addi(const uint32_t r1, const int16_t i) noexcept {
 }
 
 void div(const uint32_t r1, const uint32_t r2) noexcept {
-    notImplementedError();
+    // Note: MIPS handles division by zero slightly differently to X86.
+    // This code is taken from Avocado's division routine.
+    const int32_t a = (int32_t) r1;
+    const int32_t b = (int32_t) r2;
+
+    if (b == 0) {
+        *gpReg_lo = (a < 0) ? 0x00000001 : 0xffffffff;
+        *gpReg_hi = a;
+    } else if (r1 == 0x80000000 && r2 == 0xffffffff) {
+        *gpReg_lo = 0x80000000;
+        *gpReg_hi = 0x00000000;
+    } else {
+        *gpReg_lo = (uint32_t)(a / b);
+        *gpReg_hi = (uint32_t)(a % b);
+    }
 }
 
 void divu(const uint32_t r1, const uint32_t r2) noexcept {
-    notImplementedError();
+    // Note: MIPS handles division by zero slightly differently to X86
+    if (r2 != 0) {
+        *gpReg_lo = r1 / r2;
+        *gpReg_hi = r1 % r2;
+    } else {
+        *gpReg_lo = 0xFFFFFFFF;
+        *gpReg_hi = r1;
+    }
 }
 
 void mult(const uint32_t r1, const uint32_t r2) noexcept {
@@ -91,28 +114,29 @@ void multu(const uint32_t r1, const uint32_t r2) noexcept {
 }
 
 uint32_t sub(const uint32_t r1, const uint32_t r2) noexcept {
-    notImplementedError();
-    return 0;
+    // Note: not detecting/handling overflow and generating hardware exceptions.
+    // Can add that later if it's required for correct behavior, but I don't think it will be.
+    return r1 - r2;
 }
 
 uint32_t lb(const uint32_t addr) noexcept {
-    return PsxVm::gpCpu->sys->readMemory8(addr);
+    return gpCpu->sys->readMemory8(addr);
 }
 
 uint32_t lbu(const uint32_t addr) noexcept {
-    return (uint32_t)(int32_t)(int8_t) PsxVm::gpCpu->sys->readMemory8(addr);
+    return (uint32_t)(int32_t)(int8_t) gpCpu->sys->readMemory8(addr);
 }
 
 uint32_t lh(const uint32_t addr) noexcept {
-    return (uint32_t)(int32_t)(int16_t) PsxVm::gpCpu->sys->readMemory16(addr);
+    return (uint32_t)(int32_t)(int16_t) gpCpu->sys->readMemory16(addr);
 }
 
 uint32_t lhu(const uint32_t addr) noexcept {
-    return PsxVm::gpCpu->sys->readMemory16(addr);
+    return gpCpu->sys->readMemory16(addr);
 }
 
 uint32_t lw(const uint32_t addr) noexcept {
-    return PsxVm::gpCpu->sys->readMemory32(addr);
+    return gpCpu->sys->readMemory32(addr);
 }
 
 uint32_t lwl(const uint32_t r1, const uint32_t addr) noexcept {
@@ -126,15 +150,15 @@ uint32_t lwr(const uint32_t r1, const uint32_t addr) noexcept {
 }
 
 void sb(const uint32_t r1, const uint32_t addr) noexcept {
-    PsxVm::gpCpu->sys->writeMemory8(addr, (uint8_t) r1);
+    gpCpu->sys->writeMemory8(addr, (uint8_t) r1);
 }
 
 void sh(const uint32_t r1, const uint32_t addr) noexcept {
-    PsxVm::gpCpu->sys->writeMemory16(addr, (uint16_t) r1);
+    gpCpu->sys->writeMemory16(addr, (uint16_t) r1);
 }
 
 void sw(const uint32_t r1, const uint32_t addr) noexcept {
-    PsxVm::gpCpu->sys->writeMemory32(addr, r1);
+    gpCpu->sys->writeMemory32(addr, r1);
 }
 
 void swl(const uint32_t r1, const uint32_t addr) noexcept {
