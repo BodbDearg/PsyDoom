769aba29fb830a2f0b0797b1c52d1ebf6a82638d
Darragh Coy <darraghcoy@runbox.com>
2019-12-27 19:41:06 -0800

    P_LoadBlocks: remove a hack which now appears no longer necessary?

--- game/Doom/Game/p_setup.cpp
+++ game/Doom/Game/p_setup.cpp
@@ -1760,10 +1760,7 @@ void P_LoadBlocks(const CdMapTbl_File file) noexcept {
                 bLoadedOk = false;
                 break;
             }
-            
-        // FIXME: Disabling to allow levels from "[GEC] Master Edition PSX Doom" to load - disabling the check appears to be harmless.
-        // Does that mean however that the GEC compression tools are incorrect bad block headers? Should perhaps follow up on this.
-        #if !PC_PSX_DOOM_MODS
+
             // Verify the decompressed size is valid
             if (fileBlock.isUncompressed == 0) {
                 // Get the decompressed size of the data following the file block header
@@ -1779,7 +1776,6 @@ void P_LoadBlocks(const CdMapTbl_File file) noexcept {
                     break;
                 }
             }
-        #endif
 
             // Advance onto the next block and make sure we haven't gone past the end of the data
             const int32_t blockSize = fileBlock.size;
