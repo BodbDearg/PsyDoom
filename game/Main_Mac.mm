f634c58624e36d42a3184e426675517fb0b0aad7
Darragh Coy <darraghcoy@runbox.com>
2020-01-25 16:48:00 -0800

    MacOS: set working directory to current .app dir if not specified (defaults to '/')

--- game/Main_Mac.mm
+++ game/Main_Mac.mm
@@ -1,10 +1,23 @@
 #include "Doom/psx_main.h"
 #include "Doom/psx_main.h"
 #include "PcPsx/ModMgr.h"
+
+#define PSX_VM_NO_REGISTER_MACROS 1
 #include "PsxVm/PsxVm.h"
+
+#include <Foundation/Foundation.h>
 
 int main(const int argc, const char** const argv) noexcept {
     @autoreleasepool {
+        // Set the working directory to the .app folder if not specified
+        NSFileManager* fileMgr = [NSFileManager defaultManager];
+        NSString* workingDir = [fileMgr currentDirectoryPath];
+        
+        if ([workingDir isEqualToString: @"/"]) {
+            NSURL* appFolder = [[[NSBundle mainBundle] bundleURL] URLByDeletingLastPathComponent];
+            [fileMgr changeCurrentDirectoryPath: appFolder.path];
+        }
+    
         // Initialize the PSX VM using the NTSC-U BIOS, NTSC-U PSXDOOM.EXE and the CDROM .cue file.
         // Also initialize the modding manager:
         if (!PsxVm::init("SCPH1001.BIN", "PSXDOOM.EXE", "Doom.cue")) {
