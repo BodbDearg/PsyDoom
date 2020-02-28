d24202062a01c85ce059e2510426b2cc195e00af
Darragh Coy <darraghcoy@runbox.com>
2020-02-27 19:39:49 -0800

    MacOS: more intelligent setting of the current working dir

--- game/Main_Mac.mm
+++ game/Main_Mac.mm
@@ -6,15 +6,26 @@
 #include "PsxVm/PsxVm.h"
 
 #include <Foundation/Foundation.h>
+#include <cstdio>
 
 int main(const int argc, const char** const argv) noexcept {
     @autoreleasepool {
-        // Set the working directory to the .app folder if not specified
+        // Set the working directory to the .app folder if we can't find Doom.cue:
         NSFileManager* fileMgr = [NSFileManager defaultManager];
         NSString* workingDir = [fileMgr currentDirectoryPath];
+        std::printf("PSYDOOM current working directory: %s\n", workingDir.UTF8String);
         
-        if ([workingDir isEqualToString: @"/"]) {
-            NSURL* appFolder = [[[NSBundle mainBundle] bundleURL] URLByDeletingLastPathComponent];
+        if (![fileMgr fileExistsAtPath: @"Doom.cue"]) {
+            NSURL* bundleURL = [[NSBundle mainBundle] bundleURL];
+            NSURL* appFolder;
+            
+            if ([bundleURL.lastPathComponent.pathExtension isEqualToString: @"app"]) {
+                appFolder = [[[NSBundle mainBundle] bundleURL] URLByDeletingLastPathComponent];
+            } else {
+                appFolder = bundleURL;
+            }
+            
+            std::printf("'Doom.cue' not found at current PSYDOOM working directory! Changing PSYDOOM working dir to: %s\n", appFolder.path.UTF8String);
             [fileMgr changeCurrentDirectoryPath: appFolder.path];
         }
     
