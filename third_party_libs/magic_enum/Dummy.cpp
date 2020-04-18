7221f432a33e36f1b61446cd4da74ef87eda388b
Darragh Coy <darraghcoy@runbox.com>
2020-04-18 13:50:07 -0700

    magic_enum : handle the header only library in cmake in a proper way (see: http://mariobadr.com/creating-a-header-only-library-with-cmake.html)

--- third_party_libs/magic_enum/Dummy.cpp
+++
@@ -1,3 +0,0 @@
-// This file exists so we can treat this header only library as a regular library in various build systems.
-// I'm just including this header to test everything is ok...
-#include <magic_enum.hpp>
