13f968fe637b1b4ed03bcc8cd314cd7d8e4d3385
Darragh Coy <darraghcoy@runbox.com>
2020-05-25 10:39:27 -0700

    Network : use try/catch around asio calls

--- game/PcPsx/Network.cpp
+++ game/PcPsx/Network.cpp
@@ -7,23 +7,38 @@
 
 #include <asio.hpp>
 #include <memory>
+#include <cstdio>
 
 static std::unique_ptr<asio::io_context>        gpIoContext;
 static std::unique_ptr<asio::ip::tcp::socket>   gpSocket;
 
 // FIXME: TEMP STUFF FOR TESTING
 void Network::initForServer() noexcept {
-    gpIoContext.reset(new asio::io_context());
-    gpSocket.reset(new asio::ip::tcp::socket(*gpIoContext));
-    asio::ip::tcp::acceptor acceptor(*gpIoContext, asio::ip::tcp::endpoint(asio::ip::tcp::v6(), 1234));
-    acceptor.accept(*gpSocket);
+    try {
+        gpIoContext.reset(new asio::io_context());
+        gpSocket.reset(new asio::ip::tcp::socket(*gpIoContext));
+        asio::ip::tcp::acceptor acceptor(*gpIoContext, asio::ip::tcp::endpoint(asio::ip::tcp::v6(), 1234));
+        acceptor.accept(*gpSocket);
+    }
+    catch (...) {
+        std::printf("ERROR: Network::initForServer: error listening for an incoming connection!");
+        gpSocket.reset();
+        gpIoContext.reset();
+    }
 }
 
 // FIXME: TEMP STUFF FOR TESTING
 void Network::initForClient() noexcept {
-    gpIoContext.reset(new asio::io_context());
-    gpSocket.reset(new asio::ip::tcp::socket(*gpIoContext));
-    gpSocket->connect(asio::ip::tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 1234));
+    try {
+        gpIoContext.reset(new asio::io_context());
+        gpSocket.reset(new asio::ip::tcp::socket(*gpIoContext));
+        gpSocket->connect(asio::ip::tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 1234));
+    }
+    catch (...) {
+        std::printf("ERROR: Network::initForClient: error connecting to the server!");
+        gpSocket.reset();
+        gpIoContext.reset();
+    }
 }
 
 // FIXME: TEMP STUFF FOR TESTING
@@ -33,13 +48,19 @@ bool Network::sendBytes(const void* const pBuffer, const int32_t numBytes) noexc
         size_t bytesLeft = numBytes;
 
         while (bytesLeft > 0) {
-            const size_t bytesWritten = asio::write(*gpSocket, asio::buffer(pCurBufferBytes, (size_t) bytesLeft));
+            try {
+                const size_t bytesWritten = asio::write(*gpSocket, asio::buffer(pCurBufferBytes, (size_t) bytesLeft));
 
-            if (bytesWritten == 0)
+                if (bytesWritten == 0)
+                    return false;
+
+                pCurBufferBytes += bytesWritten;
+                bytesLeft -= bytesWritten;
+            }
+            catch (...) {
+                std::printf("ERROR: Network::sendBytes: error sending %d bytes!", numBytes);
                 return false;
-
-            pCurBufferBytes += bytesWritten;
-            bytesLeft -= bytesWritten;
+            }
         }
     }
 
@@ -53,13 +74,19 @@ bool Network::recvBytes(void* pBuffer, const int32_t numBytes) noexcept {
         size_t bytesLeft = numBytes;
 
         while (bytesLeft > 0) {
-            const size_t bytesRead = asio::read(*gpSocket, asio::buffer(pCurBufferBytes, (size_t) bytesLeft));
+            try {
+                const size_t bytesRead = asio::read(*gpSocket, asio::buffer(pCurBufferBytes, (size_t) bytesLeft));
 
-            if (bytesRead == 0)
+                if (bytesRead == 0)
+                    return false;
+
+                pCurBufferBytes += bytesRead;
+                bytesLeft -= bytesRead;
+            }
+            catch (...) {
+                std::printf("ERROR: Network::recvBytes: error receiving %d bytes!", numBytes);
                 return false;
-
-            pCurBufferBytes += bytesRead;
-            bytesLeft -= bytesRead;
+            }
         }
     }
 
