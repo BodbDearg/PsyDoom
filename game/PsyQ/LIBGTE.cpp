9c5fe5527949e36525ac6c3f99821d70a61cccf8
Darragh Coy <darraghcoy@runbox.com>
2020-05-29 00:00:22 -0700

    LIBGTE: get rid of all use of Avocado's GTE and just use simple matrix multiplication. This will remove some dependencies on the MIPS CPU.

--- game/PsyQ/LIBGTE.cpp
+++ game/PsyQ/LIBGTE.cpp
@@ -4,101 +4,73 @@
 //------------------------------------------------------------------------------------------------------------------------------------------
 #include "LIBGTE.h"
 
-#include "LIBAPI.h"
-#include "PsxVm/PsxVm.h"
+#include "PcPsx/Macros.h"
 
-BEGIN_DISABLE_HEADER_WARNINGS
-    #include <cpu/cpu.h>
-    #include <cpu/gte/gte.h>
-END_DISABLE_HEADER_WARNINGS
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-// Helper: makes a 32-bit unsigned integer from the 2 given 16-bit unsigned integers.
-// The 2nd unsigned integer is the most significant.
-//------------------------------------------------------------------------------------------------------------------------------------------
-static inline uint32_t makeU32(const uint16_t u16a, const uint16_t u16b) noexcept {
-    return (uint32_t) u16a | ((uint32_t) u16b << 16);
-};
+static int16_t gGteRotMatrix[3][3];     // Emulated Geometry Transform Engine (GTE): current rotation matrix
+static int32_t gGteTransVec[3];         // Emulated Geometry Transform Engine (GTE): current translation vector
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Upload the rotation component of the given MATRIX to the GTE
 //------------------------------------------------------------------------------------------------------------------------------------------
 void LIBGTE_SetRotMatrix(const MATRIX& m) noexcept {
-    GTE& gte = PsxVm::gpCpu->gte;
-
-    gte.rotation[0][0] = m.m[0][0];
-    gte.rotation[0][1] = m.m[0][1];
-    gte.rotation[0][2] = m.m[0][2];
-    gte.rotation[1][0] = m.m[1][0];
-    gte.rotation[1][1] = m.m[1][1];
-    gte.rotation[1][2] = m.m[1][2];
-    gte.rotation[2][0] = m.m[2][0];
-    gte.rotation[2][1] = m.m[2][1];
-    gte.rotation[2][2] = m.m[2][2];
+    gGteRotMatrix[0][0] = m.m[0][0];
+    gGteRotMatrix[0][1] = m.m[0][1];
+    gGteRotMatrix[0][2] = m.m[0][2];
+    gGteRotMatrix[1][0] = m.m[1][0];
+    gGteRotMatrix[1][1] = m.m[1][1];
+    gGteRotMatrix[1][2] = m.m[1][2];
+    gGteRotMatrix[2][0] = m.m[2][0];
+    gGteRotMatrix[2][1] = m.m[2][1];
+    gGteRotMatrix[2][2] = m.m[2][2];
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Upload the translation component of the given MATRIX to the GTE
 //------------------------------------------------------------------------------------------------------------------------------------------
 void LIBGTE_SetTransMatrix(const MATRIX& m) noexcept {
-    GTE& gte = PsxVm::gpCpu->gte;
-
-    gte.translation.x = m.t[0];
-    gte.translation.y = m.t[1];
-    gte.translation.z = m.t[2];
+    gGteTransVec[0] = m.t[0];
+    gGteTransVec[1] = m.t[1];
+    gGteTransVec[2] = m.t[2];
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Sets the 'offset' values applied to geometry for the GTE for perspective projections.
-// This call doesn't really matter for DOOM.
+// This call doesn't affect DOOM in any way, so I'm not properly implementing...
 //------------------------------------------------------------------------------------------------------------------------------------------
-void LIBGTE_SetGeomOffset(const int32_t x, const int32_t y) noexcept {
-    GTE& gte = PsxVm::gpCpu->gte;
-
-    gte.of[0] = x << 16;    // The original code did these shifts, not sure why...
-    gte.of[1] = y << 16;
-}
+void LIBGTE_SetGeomOffset([[maybe_unused]] const int32_t x, [[maybe_unused]] const int32_t y) noexcept {}
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Sets the perspective projection distance 'h' from the eye to the screen.
-// This call doesn't really matter for DOOM.
+// This call doesn't affect DOOM in any way, so I'm not properly implementing...
 //------------------------------------------------------------------------------------------------------------------------------------------
-void LIBGTE_SetGeomScreen(const int32_t h) noexcept {
-    GTE& gte = PsxVm::gpCpu->gte;
-    gte.h = (uint16_t) h;
-}
+void LIBGTE_SetGeomScreen([[maybe_unused]] const int32_t h) noexcept {}
 
 //------------------------------------------------------------------------------------------------------------------------------------------
-// Initializes the geometry transformation engine.
-// Must be called before using GTE functionality.
+// Initializes the geometry transformation engine; must be called before using GTE functionality.
+// This function doesn't need to do anything for PSX DOOM - hence unimplemented here...
 //------------------------------------------------------------------------------------------------------------------------------------------
-void LIBGTE_InitGeom() noexcept {
-    // This function doesn't need to do anything for PSX DOOM.
-    // All DOOM uses the GTE for is rotations, and we set all the state we need for that with 'LIBGTE_SetRotMatrix' and 'LIBGTE_SetTransMatrix'.
-}
+void LIBGTE_InitGeom() noexcept {}
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 // Transform the given vector by the current GTE rotation matrix and translation vector.
-// GTE status flags are saved to the given output pointer.
+// GTE status flags are saved to the given output flags field.
 //------------------------------------------------------------------------------------------------------------------------------------------
 void LIBGTE_RotTrans(const SVECTOR& vecIn, VECTOR& vecOut, int32_t& flagsOut) noexcept {
-    GTE& gte = PsxVm::gpCpu->gte;
+    // Do the matrix multiplication: this results in a 32.12 format number.
+    // Technically it won't be that here on PC since we're using 64-bit numbers but it all gets truncated at the end anyway, so no difference.
+    int64_t result[3];
+    result[0] = (int64_t) gGteRotMatrix[0][0] * vecIn.vx + (int64_t) gGteRotMatrix[0][1] * vecIn.vy + (int64_t) gGteRotMatrix[0][2] * vecIn.vz;
+    result[1] = (int64_t) gGteRotMatrix[1][0] * vecIn.vx + (int64_t) gGteRotMatrix[1][1] * vecIn.vy + (int64_t) gGteRotMatrix[1][2] * vecIn.vz;
+    result[2] = (int64_t) gGteRotMatrix[2][0] * vecIn.vx + (int64_t) gGteRotMatrix[2][1] * vecIn.vy + (int64_t) gGteRotMatrix[2][2] * vecIn.vz;
+    result[0] += (int64_t) gGteTransVec[0] << 12;
+    result[1] += (int64_t) gGteTransVec[1] << 12;
+    result[2] += (int64_t) gGteTransVec[2] << 12;
 
-    // Set GTE input vector
-    gte.v[0].x = vecIn.vx;
-    gte.v[0].y = vecIn.vy;
-    gte.v[0].z = vecIn.vz;
+    // Save the result output and convert from 32.12 format back to a simple 32-bit integer
+    vecOut.vx = (int32_t)(result[0] >> 12);
+    vecOut.vy = (int32_t)(result[1] >> 12);
+    vecOut.vz = (int32_t)(result[2] >> 12);
 
-    // This setup is required to store the result correctly
-    gte.flag.reg = 0;
-    gte.sf = true;
-    gte.lm = false;
-
-    // Do the rotation and save the result
-    gte.multiplyMatrixByVector(gte.rotation, gte.v[0], gte.translation);
-
-    vecOut.vx = gte.ir[1];
-    vecOut.vy = gte.ir[2];
-    vecOut.vz = gte.ir[3];
+    // Don't care about the value of this - it's never used anywhere...
     flagsOut = 0;
 }
