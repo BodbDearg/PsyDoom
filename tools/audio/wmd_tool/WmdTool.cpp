ead639a3626d0c5093321681c624bb10135500cf
Darragh Coy <darraghcoy@runbox.com>
2020-09-26 01:55:16 -0700

    WmdTool: can now export sequences to MIDI and import sequences from MIDI

--- tools/audio/wmd_tool/WmdTool.cpp
+++ tools/audio/wmd_tool/WmdTool.cpp
@@ -3,8 +3,13 @@
 //      Utilities for converting .WMD files (Williams Module files) to JSON and visa versa.
 //      Also utilities for importing and exporting sequences from and to MIDI.
 //------------------------------------------------------------------------------------------------------------------------------------------
+#include "MidiConvert.h"
+#include "MidiTypes.h"
+#include "MidiUtils.h"
 #include "Module.h"
 #include "ModuleFileUtils.h"
+
+#include <algorithm>
 
 using namespace AudioTools;
 
@@ -27,6 +32,39 @@ Command switches and arguments:
         Convert a module file in text JSON format to the binary .WMD format used by PlayStation Doom.
         Example:
             WmdTool -json-to-wmd DOOMSND.json DOOMSND.WMD
+
+    -sequence-to-midi <INPUT JSON OR WMD FILE PATH> <SEQUENCE INDEX> <OUTPUT MIDI FILE PATH>
+        Convert a sequence in the given module to MIDI format.
+        Notes:
+            (1) The sequence is specified by its index in the array of sequences in the module.
+            (2) The input module file can be in JSON or binary .WMD format.
+            (3) The tempo for the sequence is assumed to be constant and the same for all tracks.
+                This will be the case for all tracks in the original PSX Doom.
+        Example:
+            WmdTool -sequence-to-midi DOOMSND.WMD 90 MAP01_MUSIC.MID
+            WmdTool -sequence-to-midi DOOMSND.json 91 MAP02_MUSIC.MID
+
+    -midi-to-sequence <INPUT MIDI FILE PATH> <INPUT/OUTPUT JSON OR WMD FILE PATH> <SEQUENCE INDEX> [-is-sfx]
+        Import a sequence in MIDI format to the module.
+        Notes:
+            (1) The sequence is specified by its index in the array of sequences in the module.
+            (2) The input module file can be in JSON or binary .WMD format and is saved to the same output format.
+            (3) The sequence specified must already exist in the module. If you need to create a new empty sequence
+                for a new song or sound, export the module file to JSON and add it using an appropriate JSON editor.
+            (4) The track list for the sequence is completely replaced, however certain attributes for already existing
+                tracks will be preserved to in order to support sequence updates. The preserved track attributes include:
+                    - Start patch
+                    - Start pitch bend
+                    - Start volume
+                    - Start pan
+                    - Max voices
+            (5) This tool DOES NOT assign patches to individual tracks, you must do this manually yourself.
+                Again, export the module to JSON and edit in that format to setup the patches used by each track.
+            (6) If the '-is-sfx' flag is specified then the sequence is treated as a sequence for a sound effect.
+                This affects voice priority, sound class and whether the sequence is looped (music is automatically looped).
+        Example:
+            WmdTool -midi-to-sequence MY_MIDI.MID DOOMSND.WMD 90
+            WmdTool -midi-to-sequence MY_MIDI.MID DOOMSND.JSON 1 -is-sfx
 )";
 
 static void printHelp() noexcept {
@@ -34,10 +72,68 @@ static void printHelp() noexcept {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
+// Read a module file in .json or binary .wmd format
+//------------------------------------------------------------------------------------------------------------------------------------------
+static bool readModuleFile(const char* const filePath, bool& bIsJsonModule, Module& module) noexcept {
+    // Uppercase the filename for case insensitivity
+    std::string filePathUpper = filePath;
+    std::transform(
+        filePathUpper.begin(),
+        filePathUpper.end(),
+        filePathUpper.begin(),
+        [](const char c) noexcept { return (char) ::toupper(c); }
+    );
+
+    // Is it probably a JSON file?
+    const bool bGuessIsJsonFile = ((filePathUpper.length() >= 5) && (filePathUpper.rfind(".JSON") == filePathUpper.length() - 5));
+    bIsJsonModule = bGuessIsJsonFile;
+    
+    // Do the reading of the module file, starting with the guessed format
+    std::string readAsWmdErrorMsg;
+    std::string readAsJsonErrorMsg;
+
+    const auto readAsWmd = [&]() noexcept {
+        if (ModuleFileUtils::readWmdFile(filePath, module, readAsWmdErrorMsg)) {
+            bIsJsonModule = false;
+            return true;
+        }
+
+        return false;
+    };
+
+    const auto readAsJson = [&]() noexcept {
+        if (ModuleFileUtils::readJsonFile(filePath, module, readAsJsonErrorMsg)) {
+            bIsJsonModule = true;
+            return true;
+        }
+
+        return false;
+    };
+
+    if (bGuessIsJsonFile) {
+        if (!readAsJson()) {
+            if (!readAsWmd()) {
+                std::printf("%s\n", readAsJsonErrorMsg.c_str());
+                return false;
+            }
+        }
+    } else {
+        if (!readAsWmd()) {
+            if (!readAsJson()) {
+                std::printf("%s\n", readAsWmdErrorMsg.c_str());
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 // Convert a module file in .WMD format to JSON format
 //------------------------------------------------------------------------------------------------------------------------------------------
 static bool convertWmdToJson(const char* const wmdFileIn, const char* const jsonFileOut) noexcept {
-    // Read the input .WMD
+    // Read the input .WMD file
     Module module = {};
     std::string errorMsg;
 
@@ -59,20 +155,97 @@ static bool convertWmdToJson(const char* const wmdFileIn, const char* const json
 // Convert a module file in JSON format to .WMD format
 //------------------------------------------------------------------------------------------------------------------------------------------
 static bool convertJsonToWmd(const char* const jsonFileIn, const char* const wmdFileOut) noexcept {
-    // Read the input .json
+    // Read the input JSON file
     Module module = {};
     std::string errorMsg;
 
     if (!ModuleFileUtils::readJsonFile(jsonFileIn, module, errorMsg)) {
         std::printf("%s\n", errorMsg.c_str());
-        return 1;
+        return false;
     }
 
-    // Write the output .WMD
+    // Write the output .WMD file
     if (!ModuleFileUtils::writeWmdFile(wmdFileOut, module, errorMsg)) {
         std::printf("%s\n", errorMsg.c_str());
-        return 1;
+        return false;
     }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Convert a sequence to MIDI format
+//------------------------------------------------------------------------------------------------------------------------------------------
+static bool convertSequenceToMidi(const char* const wmdFileIn, int sequenceIdx, const char* const midiFileOut) noexcept {
+    // Read the input module file firstly
+    Module module = {};
+    bool bIsJsonModule = {};
+
+    if (!readModuleFile(wmdFileIn, bIsJsonModule, module))
+        return false;
+
+    // Is the sequence index in range?
+    if ((sequenceIdx < 0) || (sequenceIdx >= (int) module.sequences.size())) {
+        std::printf("Invalid sequence index within the module specified! Valid range is 0-%zu!\n", module.sequences.size());
+        return false;
+    }
+
+    // Convert the sequence to a MIDI file
+    MidiFile midiFile = {};
+    MidiConvert::sequenceToMidi(module.sequences[sequenceIdx], midiFile);
+
+    // Save the MIDI file
+    if (!MidiUtils::writeMidiFile(midiFileOut, midiFile)) {
+        std::printf("Error! Failed to write to the output .midi file '%s'! Is that file path writable or is the disk full?\n", midiFileOut);
+        return false;
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Import a MIDI file to a sequence
+//------------------------------------------------------------------------------------------------------------------------------------------
+static bool convertMidiToSequence(const char* const midiFileIn, const char* const moduleFilePath, const int sequenceIdx, const bool bIsSfx) noexcept {
+    // Read the input MIDI file firstly
+    std::string errorMsg;
+    MidiFile midiFile = {};
+
+    if (!MidiUtils::readMidiFile(midiFileIn, midiFile, errorMsg)) {
+        std::printf("%s\n", errorMsg.c_str());
+        return false;
+    }
+
+    // Then read the module file
+    Module module = {};
+    bool bIsJsonModule = {};
+
+    if (!readModuleFile(moduleFilePath, bIsJsonModule, module))
+        return false;
+
+    // Is the sequence index in range?
+    if ((sequenceIdx < 0) || (sequenceIdx >= (int) module.sequences.size())) {
+        std::printf("Invalid sequence index within the module specified! Valid range is 0-%zu!\n", module.sequences.size());
+        return false;
+    }
+
+    // Convert the MIDI to a sequence
+    MidiConvert::midiToSequence(midiFile, module.sequences[sequenceIdx], (!bIsSfx));
+
+    // Save the module file
+    if (bIsJsonModule) {
+        if (!ModuleFileUtils::writeJsonFile(moduleFilePath, module, errorMsg)) {
+            std::printf("%s\n", errorMsg.c_str());
+            return false;
+        }
+    } else {
+        if (!ModuleFileUtils::writeWmdFile(moduleFilePath, module, errorMsg)) {
+            std::printf("%s\n", errorMsg.c_str());
+            return false;
+        }
+    }
+
+    return true;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -100,6 +273,53 @@ int main(int argc, const char* const argv[]) noexcept {
             const char* const jsonFilePath = argv[2];
             const char* const wmdFilePath = argv[3];
             return (convertJsonToWmd(jsonFilePath, wmdFilePath)) ? 0 : 1;
+        }
+    }
+    else if (std::strcmp(cmdSwitch, "-sequence-to-midi") == 0) {
+        if (argc == 5) {
+            const char* const moduleFilePath = argv[2];
+            const char* const sequenceIdxStr = argv[3];
+            const char* const midiFilePath = argv[4];
+            int sequenceIdx = {};
+
+            try {
+                sequenceIdx = std::stoi(sequenceIdxStr);
+            } catch (...) {
+                printHelp();
+                return 1;
+            }
+
+            return (convertSequenceToMidi(moduleFilePath, sequenceIdx, midiFilePath)) ? 0 : 1;
+        }
+    }
+    else if (std::strcmp(cmdSwitch, "-midi-to-sequence") == 0) {
+        if ((argc == 5) || (argc == 6)) {
+            const char* const midiFilePath = argv[2];
+            const char* const moduleFilePath = argv[3];
+            const char* const sequenceIdxStr = argv[4];
+            int sequenceIdx = {};
+
+            try {
+                sequenceIdx = std::stoi(sequenceIdxStr);
+            } catch (...) {
+                printHelp();
+                return 1;
+            }
+
+            bool bIsSfx = false;
+
+            if (argc == 6) {
+                const char* const extraSwitch = argv[5];
+
+                if (std::strcmp(extraSwitch, "-is-sfx") == 0) {
+                    bIsSfx = true;
+                } else {
+                    printHelp();
+                    return 1;
+                }
+            }
+
+            return (convertMidiToSequence(midiFilePath, moduleFilePath, sequenceIdx, bIsSfx)) ? 0 : 1;
         }
     }
 
