e8b3245481fdfa2b53a7533a9b2302f1a1128f98
Darragh Coy <darraghcoy@runbox.com>
2022-02-01 22:13:34 -0800

    Demo results: refactor to support multiplayer demos

--- game/PsyDoom/DemoResult.cpp
+++ game/PsyDoom/DemoResult.cpp
@@ -25,118 +25,6 @@ BEGIN_NAMESPACE(DemoResult)
 static const rapidjson::Value JSON_NULL(rapidjson::kNullType);
 
 //------------------------------------------------------------------------------------------------------------------------------------------
-// Save the demo result consisting of the the player's main attributes to the given json file.
-// Returns 'false' on failure to save.
-//------------------------------------------------------------------------------------------------------------------------------------------
-bool saveToJsonFile(const char* const jsonFilePath) noexcept {
-    // Create the json document
-    rapidjson::Document document;
-    rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
-    document.SetObject();
-
-    // Add the player to the document
-    {
-        player_t& player = gPlayers[0];
-        mobj_t& mobj = *player.mo;
-
-        rapidjson::Value playerJson(rapidjson::kObjectType);
-
-        // Position angle and momentum
-        playerJson.AddMember("x", mobj.x, allocator);
-        playerJson.AddMember("y", mobj.y, allocator);
-        playerJson.AddMember("z", mobj.z, allocator);
-        playerJson.AddMember("angle", mobj.angle, allocator);
-        playerJson.AddMember("momx", mobj.momx, allocator);
-        playerJson.AddMember("momy", mobj.momy, allocator);
-        playerJson.AddMember("momz", mobj.momz, allocator);
-
-        // Health and armor
-        playerJson.AddMember("health", player.health, allocator);
-        playerJson.AddMember("armorpoints", player.armorpoints, allocator);
-        playerJson.AddMember("armortype", player.armortype, allocator);
-
-        // How long left for each power
-        {
-            rapidjson::Value powersJson(rapidjson::kArrayType);
-
-            for (int32_t i = 0; i < NUMPOWERS; ++i) {
-                powersJson.PushBack(player.powers[i], allocator);
-            }
-
-            playerJson.AddMember("powers", powersJson, allocator);
-        }
-
-        // Which keycards are owned
-        {
-            rapidjson::Value cardsJson(rapidjson::kArrayType);
-
-            for (int32_t i = 0; i < NUMCARDS; ++i) {
-                cardsJson.PushBack((int32_t) player.cards[i], allocator);   // N.B: serialize as int
-            }
-
-            playerJson.AddMember("cards", cardsJson, allocator);
-        }
-
-        // Backpack ownership and equipped weapon
-        playerJson.AddMember("backpack", (int32_t) player.backpack, allocator);     // N.B: serialize as int
-        playerJson.AddMember("readyweapon", player.readyweapon, allocator);
-
-        // Which weapons are owned
-        {
-            rapidjson::Value weaponownedJson(rapidjson::kArrayType);
-
-            for (int32_t i = 0; i < NUMWEAPONS; ++i) {
-                weaponownedJson.PushBack((int32_t) player.weaponowned[i], allocator);   // N.B: serialize as int
-            }
-
-            playerJson.AddMember("weaponowned", weaponownedJson, allocator);
-        }
-
-        // Ammo amounts
-        {
-            rapidjson::Value ammoJson(rapidjson::kArrayType);
-
-            for (int32_t i = 0; i < NUMAMMO; ++i) {
-                ammoJson.PushBack(player.ammo[i], allocator);
-            }
-
-            playerJson.AddMember("ammo", ammoJson, allocator);
-        }
-
-        // Level stats
-        playerJson.AddMember("killcount", player.killcount, allocator);
-        playerJson.AddMember("itemcount", player.itemcount, allocator);
-        playerJson.AddMember("secretcount", player.secretcount, allocator);
-
-        // Add the player to the document
-        document.AddMember("player", playerJson, allocator);
-    }
-
-    // Write the result to the given file
-    std::FILE* const pFile = std::fopen(jsonFilePath, "w");
-
-    if (!pFile)
-        return false;
-
-    auto closeFile = finally([&]() noexcept {
-        std::fflush(pFile);
-        std::fclose(pFile);
-    });
-
-    try {
-        char writeBuffer[4096];
-        rapidjson::FileWriteStream writeStream(pFile, writeBuffer, C_ARRAY_SIZE(writeBuffer));
-        rapidjson::PrettyWriter<rapidjson::FileWriteStream> fileWriter(writeStream);
-        document.Accept(fileWriter);
-    } catch (...) {
-        return false;
-    }
-
-    // All good if we get to here!
-    return true;
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------
 // Return the specified field from the given json object.
 // Returns a 'null' json value on failure.
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -192,31 +80,108 @@ static bool verifyJsonArrayFieldMatches(
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
-// Verify the current demo result consisting of the the player's main attributes matches the one stored in the given json file.
-// Returns 'true' if the result matches.
+// Serializes and adds the player to the specified json object (or document) via the given member name
 //------------------------------------------------------------------------------------------------------------------------------------------
-bool verifyMatchesJsonFileResult(const char* const jsonFilePath) noexcept {
-    // Read the input json file
-    const FileData fileData = FileUtils::getContentsOfFile(jsonFilePath, 8, std::byte(0));
+static void addPlayerToJson(
+    rapidjson::Value& jsonRoot,
+    rapidjson::Document::AllocatorType& jsonAllocator,
+    player_t& player,
+    const char* const jsonMemberName
+) noexcept {
+    // Position angle and momentum
+    rapidjson::Value playerJson(rapidjson::kObjectType);
 
-    if (!fileData.bytes)
+    {
+        mobj_t& mobj = *player.mo;
+        playerJson.AddMember("x", mobj.x, jsonAllocator);
+        playerJson.AddMember("y", mobj.y, jsonAllocator);
+        playerJson.AddMember("z", mobj.z, jsonAllocator);
+        playerJson.AddMember("angle", mobj.angle, jsonAllocator);
+        playerJson.AddMember("momx", mobj.momx, jsonAllocator);
+        playerJson.AddMember("momy", mobj.momy, jsonAllocator);
+        playerJson.AddMember("momz", mobj.momz, jsonAllocator);
+    }
+
+    // Health and armor
+    playerJson.AddMember("health", player.health, jsonAllocator);
+    playerJson.AddMember("armorpoints", player.armorpoints, jsonAllocator);
+    playerJson.AddMember("armortype", player.armortype, jsonAllocator);
+
+    // How long left for each power
+    {
+        rapidjson::Value powersJson(rapidjson::kArrayType);
+
+        for (int32_t i = 0; i < NUMPOWERS; ++i) {
+            powersJson.PushBack(player.powers[i], jsonAllocator);
+        }
+
+        playerJson.AddMember("powers", powersJson, jsonAllocator);
+    }
+
+    // Which keycards are owned
+    {
+        rapidjson::Value cardsJson(rapidjson::kArrayType);
+
+        for (int32_t i = 0; i < NUMCARDS; ++i) {
+            cardsJson.PushBack((int32_t) player.cards[i], jsonAllocator);   // N.B: serialize as int
+        }
+
+        playerJson.AddMember("cards", cardsJson, jsonAllocator);
+    }
+
+    // Backpack ownership and equipped weapon
+    playerJson.AddMember("backpack", (int32_t) player.backpack, jsonAllocator);     // N.B: serialize as int
+    playerJson.AddMember("readyweapon", player.readyweapon, jsonAllocator);
+
+    // Which weapons are owned
+    {
+        rapidjson::Value weaponownedJson(rapidjson::kArrayType);
+
+        for (int32_t i = 0; i < NUMWEAPONS; ++i) {
+            weaponownedJson.PushBack((int32_t) player.weaponowned[i], jsonAllocator);   // N.B: serialize as int
+        }
+
+        playerJson.AddMember("weaponowned", weaponownedJson, jsonAllocator);
+    }
+
+    // Ammo amounts
+    {
+        rapidjson::Value ammoJson(rapidjson::kArrayType);
+
+        for (int32_t i = 0; i < NUMAMMO; ++i) {
+            ammoJson.PushBack(player.ammo[i], jsonAllocator);
+        }
+
+        playerJson.AddMember("ammo", ammoJson, jsonAllocator);
+    }
+
+    // Level stats
+    playerJson.AddMember("killcount", player.killcount, jsonAllocator);
+    playerJson.AddMember("itemcount", player.itemcount, jsonAllocator);
+    playerJson.AddMember("secretcount", player.secretcount, jsonAllocator);
+
+    if (gNetGame == gt_deathmatch) {
+        playerJson.AddMember("frags", player.frags, jsonAllocator);
+    }
+
+    // Add the player to the document
+    {
+        rapidjson::Value rapidName(jsonMemberName, jsonAllocator);
+        jsonRoot.AddMember(rapidName, playerJson, jsonAllocator);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Verifies that the specified player matches the json result in the given json object
+//------------------------------------------------------------------------------------------------------------------------------------------
+static bool verifyPlayerMatchesJson(player_t& player, const rapidjson::Value& playerJson) noexcept {
+    // A null object can never match!
+    if (playerJson.IsNull())
         return false;
 
-    // Parse the json
-    rapidjson::Document document;
-
-    if (document.ParseInsitu((char*) fileData.bytes.get()).HasParseError())
-        return false;
-
-    // Validate everything
-    if (!document.HasMember("player"))
-        return false;
-
-    // Validate the demo result
-    player_t& player = gPlayers[0];
+    // Check all the fields of interest:
     mobj_t& mobj = *player.mo;
-
-    rapidjson::Value& playerJson = document["player"];
+    const bool bDeathmatch = (gNetGame == gt_deathmatch);
 
     return (
         verifyJsonFieldMatches(playerJson, "x", mobj.x.value) &&
@@ -237,10 +202,79 @@ bool verifyMatchesJsonFileResult(const char* const jsonFilePath) noexcept {
         verifyJsonArrayFieldMatches<int32_t>(playerJson, "ammo", player.ammo, NUMAMMO) &&
         verifyJsonFieldMatches(playerJson, "killcount", player.killcount) &&
         verifyJsonFieldMatches(playerJson, "itemcount", player.itemcount) &&
-        verifyJsonFieldMatches(playerJson, "secretcount", player.secretcount)
+        verifyJsonFieldMatches(playerJson, "secretcount", player.secretcount) &&
+        ((!bDeathmatch) || verifyJsonFieldMatches(playerJson, "frags", player.frags))
     );
+}
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Save the demo result consisting of the the player's main attributes to the given json file.
+// Returns 'false' on failure to save.
+//------------------------------------------------------------------------------------------------------------------------------------------
+bool saveToJsonFile(const char* const jsonFilePath) noexcept {
+    // Create the json document
+    rapidjson::Document document;
+    rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+    document.SetObject();
+
+    // Are we dealing with a single player or multiplayer game? (affects naming)
+    if (gNetGame == gt_single) {
+        addPlayerToJson(document, allocator, gPlayers[0], "player");
+    } else {
+        addPlayerToJson(document, allocator, gPlayers[0], "player1");
+        addPlayerToJson(document, allocator, gPlayers[1], "player2");
+    }
+
+    // Write the result to the given file
+    std::FILE* const pFile = std::fopen(jsonFilePath, "w");
+
+    if (!pFile)
+        return false;
+
+    auto closeFile = finally([&]() noexcept {
+        std::fflush(pFile);
+        std::fclose(pFile);
+    });
+
+    try {
+        char writeBuffer[4096];
+        rapidjson::FileWriteStream writeStream(pFile, writeBuffer, C_ARRAY_SIZE(writeBuffer));
+        rapidjson::PrettyWriter<rapidjson::FileWriteStream> fileWriter(writeStream);
+        document.Accept(fileWriter);
+    } catch (...) {
+        return false;
+    }
+
+    // All good if we get to here!
     return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Verify the current demo result consisting of the the player's main attributes matches the one stored in the given json file.
+// Returns 'true' if the result matches.
+//------------------------------------------------------------------------------------------------------------------------------------------
+bool verifyMatchesJsonFileResult(const char* const jsonFilePath) noexcept {
+    // Read the input json file
+    const FileData fileData = FileUtils::getContentsOfFile(jsonFilePath, 8, std::byte(0));
+
+    if (!fileData.bytes)
+        return false;
+
+    // Parse the json
+    rapidjson::Document document;
+
+    if (document.ParseInsitu((char*) fileData.bytes.get()).HasParseError())
+        return false;
+
+    // Validate the demo result
+    if (gNetGame == gt_single) {
+        return verifyPlayerMatchesJson(gPlayers[0], document["player"]);
+    } else {
+        return (
+            verifyPlayerMatchesJson(gPlayers[0], document["player1"]) &&
+            verifyPlayerMatchesJson(gPlayers[1], document["player2"])
+        );
+    }
 }
 
 END_NAMESPACE(EndResult)
