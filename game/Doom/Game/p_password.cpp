c5433f2f71d103a61c67e62f59b34ce5bad66b6c
Darragh Coy <darraghcoy@runbox.com>
2022-04-06 10:09:40 -0700

    Passwords: fix a bug that allowed passwords with invalid map numbers to be entered for 'Final Doom'. Also fixes some passwords not working for the GEC Master Edition.

--- game/Doom/Game/p_password.cpp
+++ game/Doom/Game/p_password.cpp
@@ -204,7 +204,7 @@ bool P_ProcessPassword(const uint8_t pPasswordIn[10], int32_t& mapNumOut, skill_
 
     mapNumOut = mapNum;
 
-    if ((mapNum == 0) || (mapNum > 59))
+    if ((mapNum <= 0) || (mapNum > Game::getNumMaps()))
         return false;
 
     skillOut = (skill_t)(pwdata[0] & 3);
