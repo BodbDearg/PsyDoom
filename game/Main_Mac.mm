5563f6d481e5c201a19e98d54ca13f8261016368
Darragh Coy <darraghcoy@runbox.com>
2022-09-02 00:57:33 -0700

    MacOS: remove code setting the current working directory on startup. This won't be helpful anymore to locate .cue files in the same folder as the app on modern MacOS versions due to OS sandboxing/isolation protections.

--- game/Main_Mac.mm
+++ game/Main_Mac.mm
@@ -1,30 +1,8 @@
 #include "Doom/psx_main.h"
 #include "PsyDoom/Launcher/Launcher.h"
 
-#include <Foundation/Foundation.h>
-#include <cstdio>
-
 int main(const int argc, const char* const* const argv) noexcept {
     @autoreleasepool {
-        // Set the working directory to the .app folder if we can't find Doom.cue:
-        NSFileManager* fileMgr = [NSFileManager defaultManager];
-        NSString* workingDir = [fileMgr currentDirectoryPath];
-        std::printf("PsyDoom current working directory: %s\n", workingDir.UTF8String);
-
-        if (![fileMgr fileExistsAtPath: @"Doom.cue"]) {
-            NSURL* bundleURL = [[NSBundle mainBundle] bundleURL];
-            NSURL* appFolder;
-
-            if ([bundleURL.lastPathComponent.pathExtension isEqualToString: @"app"]) {
-                appFolder = [[[NSBundle mainBundle] bundleURL] URLByDeletingLastPathComponent];
-            } else {
-                appFolder = bundleURL;
-            }
-
-            std::printf("'Doom.cue' not found at current PsyDoom working directory! Changing PsyDoom working dir to: %s\n", appFolder.path.UTF8String);
-            [fileMgr changeCurrentDirectoryPath: appFolder.path];
-        }
-
         #if PSYDOOM_LAUNCHER
             return Launcher::launcherMain(argc, argv);
         #else
