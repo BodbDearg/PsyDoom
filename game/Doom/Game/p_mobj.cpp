6ccc3c2d5e4e871d99d0f2dc371197838d7eb666
Damedade <144029086+Damedade@users.noreply.github.com>
2026-09-08 18:04:15 +0100

    Change game mode check to use gNetGame

--- game/Doom/Game/p_mobj.cpp
+++ game/Doom/Game/p_mobj.cpp
@@ -489,7 +489,7 @@ void P_SpawnMapThing(const mapthing_t& mapthing) noexcept {
     mobj.spawnangle = mapthing.angle;
 
         // Custom Randomiser Backport
-    if (gameinfo.gamemode == SafetyRandomizer) {
+    if (gNetGame == SafetyRandomizer) {
         const mobjinfo_t& checkInfo = gMobjInfo[thingType];
         
         // Only randomize actual enemies (things that count toward your kill total)
